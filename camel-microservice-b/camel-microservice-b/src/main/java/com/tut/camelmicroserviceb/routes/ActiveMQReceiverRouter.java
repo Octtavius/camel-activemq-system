5da02e15ca59ad960b7c3705a990c122636bc3e0
Octavian <octavian.mv@gmail.com>
2022-01-26 22:01:30 +0000

    unmarshal json file into java Bean. add jackson dependency

--- camel-microservice-b/camel-microservice-b/src/main/java/com/tut/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
+++ camel-microservice-b/camel-microservice-b/src/main/java/com/tut/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
@@ -1,6 +1,8 @@
 package com.tut.camelmicroserviceb.routes;
 
+import com.tut.camelmicroserviceb.beans.CurrencyExchangeBean;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +10,7 @@ public class ActiveMQReceiverRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+        .unmarshal().json(JsonLibrary.Jackson, CurrencyExchangeBean.class)
         .to("log:received-message-from-activemq");
     }
 }
